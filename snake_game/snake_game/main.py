950ed35336e9e8e24e87563f767a709dd8cc0b0d
Nikitenko Daryna <56389524+NikitenkoDaryna@users.noreply.github.com>
2021-01-18 22:14:09 +0200

    Update main.py

--- snake_game/snake_game/main.py
+++ snake_game/snake_game/main.py
@@ -38,10 +38,8 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
     # Detect collision with snake segment.
-    for segment in snake.snake_segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+        for segment in snake.snake_segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 screen.exitonclick()
